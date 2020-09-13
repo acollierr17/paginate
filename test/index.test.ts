1f4a4e7846973c485f5acde5feb9061df0a4af58
acollierr17 <gamemaniac101@gmail.com>
2020-09-13 13:19:43 -0400

    :white_check_mark: Add in test for new paginate methods
    Signed-off-by: acollierr17 <gamemaniac101@gmail.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,28 +13,61 @@ describe('test the constructor', () => {
     }).toThrowError('You must pass in a valid number for the page size!');
   });
 
+  it('should never error', () => {
+    expect(() => {
+      new Paginate([1, 2, 3], 3);
+    }).not.toThrowError();
+  });
+});
+
+describe('Paginate#setArray', () => {
+  it('should error if the array parameter is not an array', () => {
+    expect(() => {
+      new Paginate()
+        .setArray({ foo: 'bar' } as any);
+    }).toThrowError('You must pass in a valid Array object!');
+  });
+
   it('should error if no elements in array', () => {
     expect(() => {
-      new Paginate([], 2);
+      new Paginate()
+        .setArray([]);
     }).toThrowError('There must be at least (1) one element in the array!');
+  });
+
+  it('should be truthy if valid array is provided', () => {
+    const paginated = new Paginate();
+    expect(paginated.setArray([1, 2, 3])).toBeTruthy();
+  });
+});
+
+describe('Paginate#setSize', () => {
+  it('should error if the size parameter is not a number', () => {
+    expect(() => {
+      new Paginate()
+        .setSize('2' as any);
+    }).toThrowError('You must pass in a valid number for the page size!');
   });
 
   it('should error if the size per page is less than one', () => {
     expect(() => {
-      new Paginate([1, 2, 3], 0);
+      new Paginate()
+        .setSize(-1);
     }).toThrowError('The size of elements per page must be at least (1) one!');
   });
 
   it('should error if size per page is greater than array length', () => {
     expect(() => {
-      new Paginate([1, 2, 3], 4);
+      new Paginate()
+        .setArray([1, 2, 3])
+        .setSize(4);
     }).toThrowError('The size of elements per page cannot be greater than the elements in the array!');
   });
 
-  it('should never error', () => {
-    expect(() => {
-      new Paginate([1, 2, 3], 3);
-    }).not.toThrowError();
+  it('should be truthy if valid size is provided', () => {
+    const paginated = new Paginate();
+    paginated.setArray([1, 2, 3]);
+    expect(paginated.setSize(3)).toBeTruthy();
   });
 });
 
